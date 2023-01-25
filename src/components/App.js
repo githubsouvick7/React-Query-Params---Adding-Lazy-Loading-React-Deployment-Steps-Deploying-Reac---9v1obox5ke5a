a6cfadb1dc84ed1ca3cb0af0ab8ea9bba8106dce
Souvick Chakraborty <hellosouvickk@gmail.com>
2023-01-25 12:18:50 +0530

    2nd commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React,{ useState, useEffect} from 'react'
 import '../styles/App.css';
 
 const App = () => {
   //Write your code here
+  const [employee, setEmployee] = useState({});
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const id = searchParams.get("id");
+    const name = searchParams.get("name");
+    const designation = searchParams.get("designation");
+    setEmployee({ id, name, designation });
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="details">
+        Employee {employee.id} named {employee.name} works as {employee.designation}
+      </div>
+    </div>
+  );
 }
 
 export default App;
